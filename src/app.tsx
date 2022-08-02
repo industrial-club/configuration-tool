6cbb24714cd2d80354a8ff64e686706d7101543a
bhabgs <bhabgs@aliyun.com>
2022-08-02 16:36:05 +0800

    xas

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,18 @@
-import { defineComponent, onMounted } from "vue";
-// import canvasEditor from "./canvasEditor";
+import { defineComponent, onMounted, ref } from "vue";
+import inl from "inl-ui";
 import canvasEditor from "./mtip-it";
 import "./index.less";
 
 export default defineComponent({
   components: { canvasEditor },
   setup(props, content) {
-    return () => <canvasEditor />;
+    const state = ref(false);
+    const login = new inl.utils.login();
+    login.getTokenByCode().then((res) => {
+      if (res.token) {
+        state.value = true;
+      }
+    });
+    return () => (state.value ? <canvasEditor /> : "");
   },
 });
